3dd78d4bf1aa708d1e0025a094f94840505fc654
Alessandro <ale.loguzzo@gmail.com>
2018-09-19 13:48:12 +0200

    dev_10

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ server.post('/getMovies',function (req,res)  {
                 fulfillmentText: 'Prima opzione: ' + '\nCategoria veicolo: ' + bodyJSON['0']['categoryName'] + '\nPrezzo: € ' + bodyJSON['0']['price'] + '\nInfo: ' + bodyJSON['0']['info']
             });*/
 
-            return res.json({
+            /*return res.json({
                   "payload": {
                     "google": {
                       "expectUserResponse": true,
@@ -72,7 +72,57 @@ server.post('/getMovies',function (req,res)  {
                   }
             });
 
-        });
+        });*/
+
+            return res.json( {
+              "payload": {
+                "google": {
+                  "expectUserResponse": true,
+                  "richResponse": {
+                    "items": [
+                      {
+                        "simpleResponse": {
+                          "textToSpeech": "Choose a item"
+                        }
+                      }
+                    ]
+                  },
+                  "systemIntent": {
+                    "intent": "actions.intent.OPTION",
+                    "data": {
+                      "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+                      "listSelect": {
+                        "title": "Hello",
+                        "items": [
+                          {
+                            "optionInfo": {
+                              "key": "first title key"
+                            },
+                            "description": "first description",
+                            "image": {
+                              "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                              "accessibilityText": "first alt"
+                            },
+                            "title": "first title"
+                          },
+                          {
+                            "optionInfo": {
+                              "key": "second"
+                            },
+                            "description": "second description",
+                            "image": {
+                              "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                              "accessibilityText": "second alt"
+                            },
+                            "title": "second title"
+                          }
+                        ]
+                      }
+                    }
+                  }
+                }
+              }
+            });
 
         
     }
