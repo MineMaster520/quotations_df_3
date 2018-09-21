7e0564b6a22c234387eafe1f777b3f3fe2b299bb
Alessandro <ale.loguzzo@gmail.com>
2018-09-21 12:35:29 +0200

    dev

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server = express();
 server.use(bodyParser.json());
 server.post('/webhook',function (req,res)  {
 
-  var numPasseggeri = 100;
+  var numPasseggeri = "ciao";
 
     if(req.body.queryResult.intent.displayName == "Agente-Mail") {
 
