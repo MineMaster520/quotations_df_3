9ede73af9eb6dc2017e17cacdef4f02e05a3c2c3
Alessandro <ale.loguzzo@gmail.com>
2018-09-21 11:25:28 +0200

    dev

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server = express();
 server.use(bodyParser.json());
 server.post('/webhook',function (req,res)  {
 
-    if(req.body.queryResult.intent.displayName == "Agente-Mail" && req.body.queryResult.allRequiredParamsPresent == true) {
+    if(req.body.queryResult.allRequiredParamsPresent) {
 
         superagent.get('http://quote.moveolux.com:88/home/testquote?from=milano&to=roma&day=13/12/2018&time=10:00')
         .end((err, resp) => {
