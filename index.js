a52d8964f7b8a414c6bb35b81ee7be04133ddcd2
Alessandro <ale.loguzzo@gmail.com>
2018-09-25 12:48:56 +0200

    asd

--- index.js
+++ index.js
@@ -188,8 +188,8 @@ server.post('/webhook',function (req,res)  {
                 var respBody2 = resp.text;
                 var bodyJSON2 = JSON.parse(respBody2);
 
-                distanzaPerc = resp['km'];
-                tempoPerc = resp['time'];
+                distanzaPerc = bodyJSON2['km'];
+                tempoPerc = bodyJSON2['time'];
               });
               respJSON2 = {
                 "payload": {
