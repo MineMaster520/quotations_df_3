7976e58bd3fe7be3a4f126e64086254a7c51dc5a
Alessandro <ale.loguzzo@gmail.com>
2018-09-27 15:23:57 +0200

    asd

--- index.js
+++ index.js
@@ -298,9 +298,9 @@ server.post('/webhook',function (req,res)  {
     var partStringN = partString.replace(/\s/g, "+");
     var destStringN = destString.replace(/\s/g, "+");
 
-    var mapMatrixUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + partCity + ",ITALIA&destinations=" + destCity + ",ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
+    var mapMatrixUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + partStringN + ",ITALIA&destinations=" + destStringN + ",ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
      //Retrieve points path road
-    var urlPoints= "https://maps.googleapis.com/maps/api/directions/json?origin=Monza,ITALIA&destination=Milano,ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
+    var urlPoints= "https://maps.googleapis.com/maps/api/directions/json?origin=" + partStringN + ",ITALIA&destination=" + destStringN + ",ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
 
 
     superagent.get(mapMatrixUrl).end((err3, resp3) => {
@@ -337,7 +337,7 @@ server.post('/webhook',function (req,res)  {
                           {
                             "basicCard": {
                               "title": "Conferma dati",
-                              "formattedText": "**Partenza**: " + partStringN + "\n  \n**Destinazione**: " + destStringN + "\n  \n**Data**: " + dataPart + "\n  \n**Ora**: " + oraPart + "\n  \n**Distanza**: " + distanzaPercApi + ", **Durata prevista**: " + tempoPercApi + "\n  \n**Passeggeri**: " + numPass + "\n  \n**Email**: " + mail,
+                              "formattedText": "**Partenza**: " + partString + "\n  \n**Destinazione**: " + destString + "\n  \n**Data**: " + dataPart + "\n  \n**Ora**: " + oraPart + "\n  \n**Distanza**: " + distanzaPercApi + ", **Durata prevista**: " + tempoPercApi + "\n  \n**Passeggeri**: " + numPass + "\n  \n**Email**: " + mail,
                               "image": {
                                 "url": mapPointsUrl,
                                 "accessibilityText": "Map"
