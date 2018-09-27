6ccaec8648e356501cf166526302ecfc50ebc577
Alessandro <ale.loguzzo@gmail.com>
2018-09-27 14:43:01 +0200

    asd

--- index.js
+++ index.js
@@ -298,9 +298,9 @@ server.post('/webhook',function (req,res)  {
     var partStringN = partString.replace(/ /g, "+");
     var destStringN = destString.replace(/ /g, "+");
 
-    var mapMatrixUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + partCity + ",ITALIA&destinations=" + destCity + ",ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
+    var mapMatrixUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + partStringN + ",ITALIA&destinations=" + destStringN + ",ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
      //Retrieve points path road
-    var urlPoints= "https://maps.googleapis.com/maps/api/directions/json?origin=" + partCity + ",ITALIA&destination=" + destCity + ",ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
+    var urlPoints= "https://maps.googleapis.com/maps/api/directions/json?origin=Monza,ITALIA&destination=Milano,ITALIA&key=AIzaSyCIeu1JhV_R4AGNnaiv74gHF5t6b-ilVhU";
 
 
     superagent.get(mapMatrixUrl).end((err3, resp3) => {
